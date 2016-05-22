a795f06a2b80d39a5b5ad1b79b1eab39a609a696
Caspar Brenneisen <caspar.brenneisen@hotmail.ch>
2016-05-22 21:58:20 +0200

    Added licence
    I added my licence to every file

--- language.func.php
+++ language.func.php
@@ -1,4 +1,18 @@
 <?php
+ /*
+  * ----------------------------------------------------------------------------------
+  * This is the "DIFFERENT-BEER-WARE LICENSE" -> (v0.1):
+  * I (Chronyms / <chronyms@outlook.com>) wrote this Licence-File on the Basement of
+  * "THE BEER-WARE LICENSE" (Revision 42). Based on the Sense of "THE BEER-WARE LICENSE",
+  * can you do whatever you want with this stuff, as long as you didn't remove the 
+  * Licence-File / Licence-Section.
+  * The only condition is, that you say thank you, to the creator. You can therefore send
+  * a E-Mail with some nice words, send a Letter with some words and images or buy me a
+  * Beer, if we meet someday.
+  * And btw., please do crazy stuff.
+  * -----------------------------------------------------------------------------------
+  */
+
 	include("./include/config.php");
 	if(isset($_SESSION['config']['currentLanguage']))
 	{
